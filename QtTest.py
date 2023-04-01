a1954a5e8e1d73c644a69708ca0ddb99b84335f9
mimi35garry <mikel.langlais@gmail.com>
2023-04-01 18:10:27 +0200

    add QtTest.py!

---
+++ QtTest.py
@@ -0,0 +1,38 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QPushButton, QVBoxLayout, QApplication, QWidget
+from picamera2.previews.qt import QGlPicamera2
+from picamera2 import Picamera2
+from libcamera import controls
+
+picam2 = Picamera2()
+picam2.configure(picam2.create_preview_configuration())
+picam2.set_controls({"AfMode": controls.AfModeEnum.Continuous})
+
+
+def on_button_clicked():
+    button.setEnabled(False)
+    cfg = picam2.create_still_configuration()
+    picam2.switch_mode_and_capture_file(cfg, "test.jpg", signal_function=qpicamera2.
+                                        signal_done)
+
+
+def capture_done(job):
+    result = picam2.wait(job)
+    button.setEnabled(True)
+
+
+app = QApplication([])
+qpicamera2 = QGlPicamera2(picam2, width=800, height=600, keep_ar=False)
+button = QPushButton("Click to capture JPEG")
+window = QWidget()
+qpicamera2.done_signal.connect(capture_done)
+button.clicked.connect(on_button_clicked)
+layout_v = QVBoxLayout()
+layout_v.addWidget(qpicamera2)
+layout_v.addWidget(button)
+window.setWindowTitle("Qt Picamera2 App")
+window.resize(440, 280)
+window.setLayout(layout_v)
+picam2.start()
+window.show()
+app.exec()
